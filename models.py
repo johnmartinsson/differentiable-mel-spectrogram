3cb12e6dc4209af20834ba953582c9e04851435e
John Martinsson <john.martinsson@gmail.com>
2023-02-14 10:57:10 +0100

    Remove batch norm and fewer fc layers in convnet

--- models.py
+++ models.py
@@ -156,7 +156,6 @@ class MlpNet(nn.Module):
         x = F.dropout(x, p=0.2)
         x = self.fc2(x)
         return x, s
-    
 
 class ConvNet(nn.Module):
     def __init__(self, n_classes, init_sigma, device, optimized=False, size=(512, 1024), hop_length=1):
@@ -165,50 +164,29 @@ class ConvNet(nn.Module):
         
         self.device = device
         self.size = size
+
+        self.hidden_state = 32
         
-        #self.bn0 = nn.BatchNorm2d(1)
-        #self.bn1 = nn.BatchNorm2d(6)
-        #self.bn2 = nn.BatchNorm2d(16)
-        
-        self.conv1 = nn.Conv2d(1, 6, 5, padding='same')
+        self.conv1 = nn.Conv2d(1, self.hidden_state, 5, padding='same')
+        self.fc1 = nn.Linear(self.hidden_state * (size[0] // 2) * (size[1] // 2), self.hidden_state)
+        self.fc2 = nn.Linear(self.hidden_state, n_classes)
+
         self.pool = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(6, 16, 5, padding='same')
-        self.fc1 = nn.Linear(16 * (size[0] // 4) * (size[1] // 4), 32)
-        self.fc2 = nn.Linear(32, 32)
-        self.fc3 = nn.Linear(32, n_classes)
         self.dropout = nn.Dropout(p=0.2)
         
     def forward(self, x):
         # compute spectrograms
         s = self.spectrogram_layer(x)
-        #s = self.bn0(s)
-        
+
         # TODO: residual layers?
         x = self.conv1(s)
-        #x = self.bn1(x)
         x = F.relu(x)
         x = self.pool(x)
-        
-        x = self.conv2(x)
-        #x = self.bn2(x)
-        x = F.relu(x)
-        x = self.pool(x)
-        #print("1", x.shape)
 
-        #print(self.size)
-        x = x.view(-1, 16 * (self.size[0] // 4) * (self.size[1] // 4))
-        #print("1.1", x.shape)
-        x = self.dropout(x)
+        x = x.view(-1, self.hidden_state * (self.size[0] // 2) * (self.size[1] // 2))
         x = self.fc1(x)
         x = F.relu(x)
-        #print("2", x.shape)
-        
         x = self.dropout(x)
         x = self.fc2(x)
-        x = F.relu(x)
-        #print("3", x.shape)
-        
-        x = self.dropout(x)
-        x = self.fc3(x)
-        #print("4", x.shape)
+
         return x, s
