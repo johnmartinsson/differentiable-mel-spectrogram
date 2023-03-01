af53f87beab4d19ccf8f35d4241a09894498e230
John Martinsson <john.martinsson@gmail.com>
2023-03-01 19:32:30 +0100

    Adds final experiment run script.

---
+++ run_experiments.sh
@@ -0,0 +1,2 @@
+CUDA_VISIBLE_DEVICES=1 python main.py --num_samples=5 --max_epochs=1000 --name=audio_mnist
+CUDA_VISIBLE_DEVICES=1 python main.py --num_samples=5 --max_epochs=1000 --name=time_frequency
