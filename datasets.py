22968b3ca58afd852f2dc9fea16a1dfb0aad6db5
John Martinsson <john.martinsson@gmail.com>
2023-03-06 13:53:47 +0100

    Cleanup datasets.

--- datasets.py
+++ datasets.py
@@ -34,29 +34,20 @@ class GaussPulseDatasetTimeFrequency(torch.utils.data.Dataset):
         self.ys = torch.empty((n_samples), dtype=torch.long)
         self.locs = torch.zeros((n_samples, 4), dtype=torch.float64)
 
-	# maximum time-offset for pulses from center
-        # TODO: n_points / 5 ?
+	# maximum displacement limits for time-offset for pulses from center
         image_displacement = 5
-        t_max = n_points / image_displacement #sigma*s
+        t_max = n_points / image_displacement 
+        f_max = 0.5 / image_displacement 
 
-
-        f_max = 0.5 / image_displacement #0.1 #1/(np.pi*sigma) * s #0.1 #sigma*s
-
-        # TODO: lower limits
+        # lower displacement limits
         t_min = sigma
-        f_min = 0.5 * (t_min / n_points)
+        f_min = 0.5 * (t_min / n_points) # = sigma / (2N) = sigma / K = 1/(2*pi*sigma)
 
 
-        # TODO: are these reasonable values?
         sigma_scale_max = (2*t_max)/(6*sigma) + 1
 	# minimum duration scaling for pulses on center
         sigma_scale_min = 1 / sigma_scale_max 
 
-        #print("sigma_scale_min :", sigma_scale_min)
-        #print("sigma_scale_max :", sigma_scale_max)
-
-        #print("f_min = {}, f_max = {}".format(f_min, f_max))
-        #print("t_min = {}, t_max = {}".format(t_min, t_max))
 
         # generate samples
         for idx in range(n_samples):
@@ -77,9 +68,6 @@ class GaussPulseDatasetTimeFrequency(torch.utils.data.Dataset):
                 # TODO: this is an important change!!!!
                 f_offset = torch_random_uniform([f_min, f_max])
                 t_offset = torch_random_uniform([t_min, t_max])
-
-                #f_offset = torch.rand(1) * f_max
-                #t_offset = torch.rand(1) * t_max
 
             y = np.random.choice([0, 1, 2])
 
@@ -149,200 +137,6 @@ class GaussPulseDatasetTimeFrequency(torch.utils.data.Dataset):
     def __getitem__(self, idx):
         return self.xs[idx], self.ys[idx]
 
-class GaussPulseDatasetFrequency(torch.utils.data.Dataset):
-    def __init__(self, sigma, n_points, noise_std, n_samples=10000, f_center_max_offset=0, t_center_max_offset=0):
-        
-        self.xs = torch.empty((n_samples, n_points), dtype=torch.float64)
-        self.ys = torch.empty((n_samples), dtype=torch.long)
-        self.locs = torch.zeros((n_samples, 4), dtype=torch.float64)
-
-	# maximum time-offset for pulses from center
-        # TODO: n_points / 5 ?
-        image_displacement = 5
-        t_max = n_points / image_displacement #sigma*s
-
-        # TODO: what should this value be?
-        f_max = 0.5 / image_displacement #0.1 #1/(np.pi*sigma) * s #0.1 #sigma*s
-
-        # TODO: are these reasonable values?
-	# maximum duration scaling for pulses on center
-        sigma_scale_max = (2*t_max)/(6*sigma) + 1
-	# minimum duration scaling for pulses on center
-        sigma_scale_min = 1 / sigma_scale_max 
-
-        # generate samples
-        for idx in range(n_samples):
-            f_center_offset = torch_random_uniform([-f_center_max_offset, f_center_max_offset])
-            t_center_offset = torch_random_uniform([-t_center_max_offset, t_center_max_offset])
-
-            t_center = t_center_offset + torch.tensor(n_points/2, dtype=torch.float)
-            f_center = f_center_offset + 0.25
-
-            y = np.random.choice([0, 1])
-
-            if y == 0:
-                sigma_scale = torch_random_uniform([sigma_scale_min, 1.0])
-                
-                x = gauss_pulse(t_center, f_center, sigma*sigma_scale, n_points)
-
-                self.locs[idx, 0] = t_center
-                self.locs[idx, 1] = f_center
-            else:
-                f_offset = torch.rand(1) * f_max
-                f_loc_1 = f_center - f_offset
-                f_loc_2 = f_center + f_offset
-
-                x1 = gauss_pulse(t_center, f_loc_1, sigma, n_points)
-                x2 = gauss_pulse(t_center, f_loc_2, sigma, n_points)
-                x = x1 + x2
-
-                self.locs[idx, 0] = t_center
-                self.locs[idx, 1] = f_loc_1
-                self.locs[idx, 2] = t_center
-                self.locs[idx, 3] = f_loc_2
-
-            # variability
-            noise = noise_std * torch.rand(n_points)
-            amplitude_scale = torch_random_uniform([0.5, 1])
-            x = (x * amplitude_scale) + noise
-            
-            self.ys[idx] = torch.tensor(y, dtype=torch.long)
-            self.xs[idx] = x - torch.mean(x)
-    
-    def __len__(self):
-        return len(self.xs)
-    
-    def __getitem__(self, idx):
-        return self.xs[idx], self.ys[idx]
-
-class GaussPulseDatasetTime(torch.utils.data.Dataset):
-    def __init__(self, sigma, n_points, noise_std, n_samples=10000, f_center_max_offset=0, t_center_max_offset=0):
-        
-        self.xs = torch.empty((n_samples, n_points), dtype=torch.float64)
-        self.ys = torch.empty((n_samples), dtype=torch.long)
-        self.locs = torch.zeros((n_samples, 4), dtype=torch.float64)
-
-	# maximum time-offset for pulses from center
-        # TODO: n_points / 5 ?
-        image_displacement = 5
-        t_max = n_points / image_displacement #sigma*s
-
-        # TODO: what should this value be?
-        f_max = 0.5 / image_displacement #0.1 #1/(np.pi*sigma) * s #0.1 #sigma*s
-
-        # TODO: are these reasonable values?
-	# maximum duration scaling for pulses on center
-        sigma_scale_max = (2*t_max)/(6*sigma) + 1
-	# minimum duration scaling for pulses on center
-        sigma_scale_min = 1 / sigma_scale_max 
-
-        # generate samples
-        for idx in range(n_samples):
-            f_center_offset = torch_random_uniform([-f_center_max_offset, f_center_max_offset])
-            t_center_offset = torch_random_uniform([-t_center_max_offset, t_center_max_offset])
-
-            t_center = t_center_offset + torch.tensor(n_points/2, dtype=torch.float)
-            f_center = f_center_offset + 0.25
-
-            y = np.random.choice([0, 1])
-
-            if y == 0:
-                sigma_scale = torch_random_uniform([1.0, sigma_scale_max])
-                
-                x = gauss_pulse(t_center, f_center, sigma*sigma_scale, n_points)
-
-                self.locs[idx, 0] = t_center
-                self.locs[idx, 1] = f_center
-            else:
-                t_offset = torch.rand(1) * t_max
-                t_loc_1 = t_center - t_offset
-                t_loc_2 = t_center + t_offset
-
-                x1 = gauss_pulse(t_loc_1, f_center, sigma, n_points)
-                x2 = gauss_pulse(t_loc_2, f_center, sigma, n_points)
-                x = x1 + x2
-
-                self.locs[idx, 0] = t_loc_1
-                self.locs[idx, 1] = f_center
-                self.locs[idx, 2] = t_loc_2
-                self.locs[idx, 3] = f_center
-
-            # variability
-            noise = noise_std * torch.rand(n_points)
-            amplitude_scale = torch_random_uniform([0.5, 1])
-            x = (x * amplitude_scale) + noise
-            
-            self.ys[idx] = torch.tensor(y, dtype=torch.long)
-            self.xs[idx] = x - torch.mean(x)
-    
-    def __len__(self):
-        return len(self.xs)
-    
-    def __getitem__(self, idx):
-        return self.xs[idx], self.ys[idx]
-
-class GaussPulseDataset(torch.utils.data.Dataset):
-    def __init__(self, sigma, n_points, noise_std, n_samples=10000):
-        
-        self.xs = torch.empty((n_samples, n_points), dtype=torch.float64)
-        self.ys = torch.empty((n_samples), dtype=torch.long)
-        self.locs = torch.zeros((n_samples, 4), dtype=torch.float64)
-
-        d_freq = 1/(torch.pi * sigma)
-        print("freq res: ", d_freq)
-        # TODO: the factor of two should be removed for theoretical limit
-        d_time = sigma * 2
-        print("time res: ", d_time)
-        
-        f_lims = [d_freq*2, 0.5-d_freq*2]
-        print("freq lims: ", f_lims)
-        t_lims = [d_time*2, n_points-d_time*2]
-        print("time lims: ", t_lims)
-
-        # generate samples
-        for idx in range(n_samples):
-            t_loc = torch.tensor(np.random.uniform(low=t_lims[0], high=t_lims[1]), dtype=torch.float64)
-            f_loc = torch.tensor(np.random.uniform(low=f_lims[0], high=f_lims[1]), dtype=torch.float64)
-            y = np.random.choice([0, 1, 2])
-            #if y == 0:
-            #    sigma_scale = torch_random_uniform([0.8, 1.2])
-            #else:
-            sigma_scale = 1
-            x = gauss_pulse(t_loc, f_loc, sigma*sigma_scale, n_points)
-
-            scale = torch_random_uniform([0.2, 1])
-
-            self.locs[idx, 0] = t_loc
-            self.locs[idx, 1] = f_loc
-
-            if y > 0:
-                t_loc_o = t_loc
-                f_loc_o = f_loc
-                if y == 1:
-                    t_loc_o = torch.tensor(np.random.choice([t_loc+d_time, t_loc-d_time]), dtype=torch.float64)
-                if y == 2:
-                    f_loc_o = torch.tensor(np.random.choice([f_loc+d_freq, f_loc-d_freq]), dtype=torch.float64)
-                x_o = gauss_pulse(t_loc_o, f_loc_o, sigma, n_points)
-                x = x + x_o
-
-                self.locs[idx, 2] = t_loc_o
-                self.locs[idx, 3] = f_loc_o
-
-            # variability
-            noise = noise_std * torch.rand(n_points)
-            x = (x * scale) + noise
-            
-            self.ys[idx] = torch.tensor(y, dtype=torch.long)
-            self.xs[idx] = x - torch.mean(x)
-    
-    def __len__(self):
-        return len(self.xs)
-    
-    def __getitem__(self, idx):
-        return self.xs[idx], self.ys[idx]
-
-
-
 def parse_row(row):
     filename = row[0]
     fold = int(row[1])
@@ -365,6 +159,44 @@ def load_meta_data(source_dir):
     csv_file = os.path.join(source_dir, 'meta', 'esc50.csv')
     meta_data = parse_csv(csv_file)
     return meta_data
+
+class AudioMNISTDataset(torch.utils.data.Dataset):
+    # VERSION: https://doi.org/10.5281/zenodo.1342401
+    def __init__(self, source_dir):
+        # load data
+        wav_paths = glob.glob(os.path.join(source_dir, 'recordings', '*.wav'))
+
+        self.xs = []
+        self.ys = []
+
+        sample_rates = []
+        for wav_path in wav_paths:
+            audio, sr = librosa.load(wav_path, sr=None) # already 8000 Hz
+            sample_rates.append(sr)
+            target = int(os.path.basename(wav_path).split('_')[0])
+
+            if len(audio) >= 1500 and len(audio) <= 5500:
+                x = audio.copy()
+                x.resize(5500) # pad end with zeros up to max length
+                self.xs.append(x)
+                self.ys.append(target)
+
+        assert len(list(set(self.ys))) == 10 # assert 10 classes
+
+        self.xs = np.array(self.xs)
+        self.ys = np.array(self.ys)
+
+        # assert all files have the same sample rates
+        assert len(list(set(sample_rates))) == 1
+        # assert proper sample rate
+        assert sample_rates[0] == 8000
+
+        self.sample_rate = 8000
+
+    def __len__(self):
+        return len(self.xs)
+    def __getitem__(self, idx):
+        return self.xs[idx], self.ys[idx]
 
 class ESC50Dataset(torch.utils.data.Dataset):
     def __init__(self, source_dir, resample_rate=8000):
@@ -413,40 +245,4 @@ class ESC50Dataset(torch.utils.data.Dataset):
     def __getitem__(self, idx):
         return self.xs[idx], self.ys[idx]
 
-class AudioMNISTDataset(torch.utils.data.Dataset):
-    # VERSION: https://doi.org/10.5281/zenodo.1342401
-    def __init__(self, source_dir):
-        # load data
-        wav_paths = glob.glob(os.path.join(source_dir, 'recordings', '*.wav'))
 
-        self.xs = []
-        self.ys = []
-
-        sample_rates = []
-        for wav_path in wav_paths:
-            audio, sr = librosa.load(wav_path, sr=None) # already 8000 Hz
-            sample_rates.append(sr)
-            target = int(os.path.basename(wav_path).split('_')[0])
-
-            if len(audio) >= 1500 and len(audio) <= 5500:
-                x = audio.copy()
-                x.resize(5500) # pad end with zeros up to max length
-                self.xs.append(x)
-                self.ys.append(target)
-
-        assert len(list(set(self.ys))) == 10 # assert 10 classes
-
-        self.xs = np.array(self.xs)
-        self.ys = np.array(self.ys)
-
-        # assert all files have the same sample rates
-        assert len(list(set(sample_rates))) == 1
-        # assert proper sample rate
-        assert sample_rates[0] == 8000
-
-        self.sample_rate = 8000
-
-    def __len__(self):
-        return len(self.xs)
-    def __getitem__(self, idx):
-        return self.xs[idx], self.ys[idx]
