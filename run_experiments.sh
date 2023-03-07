b8b8aa61075af43ecb1fd6b703836e10a41e0138
John Martinsson <john.martinsson@gmail.com>
2023-03-07 15:55:30 +0100

    Update run_experiments.sh

--- run_experiments.sh
+++ run_experiments.sh
@@ -1,2 +1,2 @@
-CUDA_VISIBLE_DEVICES=1 python main.py --num_samples=5 --max_epochs=1000 --name=audio_mnist --ray_root_dir=$(pwd)/ray_results --data_dir=$(pwd)/data/audio-mnist
-CUDA_VISIBLE_DEVICES=1 python main.py --num_samples=5 --max_epochs=1000 --name=time_frequency --ray_root_dir=$(pwd)/ray_results --data_dir=$(pwd)/data/audio-mnist
+CUDA_VISIBLE_DEVICES=0 python main.py --num_samples=5 --max_epochs=1000 --name=audio_mnist --ray_root_dir=$(pwd)/ray_results --data_dir=$(pwd)/data/audio-mnist
+CUDA_VISIBLE_DEVICES=0 python main.py --num_samples=5 --max_epochs=1000 --name=time_frequency --ray_root_dir=$(pwd)/ray_results --data_dir=$(pwd)/data/audio-mnist
