be907deef03e654d2cee8ebec116dee281f23674
John Martinsson <john.martinsson@gmail.com>
2023-03-07 08:57:02 +0100

    Adds download data script.

---
+++ download_data.sh
@@ -0,0 +1,5 @@
+wget https://zenodo.org/record/1342401/files/Jakobovski/free-spoken-digit-dataset-v1.0.8.zip
+unzip free-spoken-digit-dataset-v1.0.8.zip
+mkdir data
+mv Jakobovski-free-spoken-digit-dataset-e9e1155/ data/audio-mnist
+rm free-spoken-digit-dataset-v1.0.8.zip
