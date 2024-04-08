d73b94f73404558a260245c8ee1a8682eede7e8f
John Martinsson <john.martinsson@ri.se>
2024-04-08 16:12:12 +0200

    Adds informative printing.

--- doit.sh
+++ doit.sh
@@ -1,11 +1,15 @@
 # download the FSD dataset
+echo "downloading all data ..."
 sh download_data.sh
 
 # run all the experiments (takes ~16h on a 2080Ti)
+echo "running all experiments ..."
 sh run_experiments.sh
 
 # run all test predictions
+echo "running all test predictions ..."
 sh run_test_predictions.sh
 
 # produce all the tables
+echo "producing all tables ..."
 python produce_tables.py --ray_results_dir=$(pwd)/ray_results/
--- download_data.sh
+++ download_data.sh
@@ -9,10 +9,12 @@ mv ESC-50/ data/esc50
 mv AudioMNIST/ data/audio_mnist
 
 # resample AudioMNIST to 8000 Hz
+echo "resample all Audio-MNIST files to 8000 Hz"
 for file in $(find ./data/audio_mnist -type f -name "*.wav"); do
     sox $file -r 8000 ${file%.wav}_8k.wav
     mv ${file%.wav}_8k.wav $file
 done
 
-# initialize and test datasets
+# initialize datasets
+echo "initialize audio datasets ..."
 python3 init_dataset.py $(pwd)/data
